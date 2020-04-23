d1373a531dd547de51094d35320db66aa5c8db70
Chloe Kudryavtsev <toast@toast.cafe>
2020-04-23 13:20:34 -0400

    add HelpOpt - special handling for --help

---
+++ help.go
@@ -0,0 +1,43 @@
+package libuconf
+
+// HelpOpt is a special Option to provide -h and --help
+type HelpOpt bool
+
+// Help adds --help and -h flags to the Option Set
+func (o *OptionSet) Help() *bool {
+	out := new(bool)
+	o.Var((*HelpOpt)(out))
+	return out
+}
+
+// ---- FlagOpt
+
+// Bool returns whether or not this option is a boolean
+func (*HelpOpt) Bool() bool {
+	return true
+}
+
+// Flag returns the long-form flag for this option
+func (r *HelpOpt) Flag() string {
+	return "help"
+}
+
+// Help returns the help string for this option
+func (r *HelpOpt) Help() string {
+	return "view this help message"
+}
+
+// ShortFlag returns the short-form flag for this option
+func (r *HelpOpt) ShortFlag() rune {
+	return 'h'
+}
+
+// ---- Setter
+
+// Set sets this option's value
+func (r *HelpOpt) Set(vv interface{}) error {
+	*r = true // if help is mentioned, it's true
+	// otherwise, this is an implicit non-fatal failure:
+	// ./app false --help false
+	return nil
+}
